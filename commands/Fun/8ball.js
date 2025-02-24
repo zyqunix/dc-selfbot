f91dfdf20a868f58943a6c111a8e616cc71f2853
zyqunix <117040076+zyqunix@users.noreply.github.com>
2025-02-24 17:02:54 +0100

    Update 8ball.js

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -4,7 +4,6 @@ module.exports = {
     execute(message, args) {
         const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely", "As I see it, yes", "Most likely", "Signs point to yes", "Yes", "My reply is no", "My sources say no", "Very doubtful"];
 
-        // Use the current timestamp as a seed for the random number generator
         const seed = new Date().getTime();
         Math.random(seed);
 
