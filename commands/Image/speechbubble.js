8ed34501821c83c9bfbffe1829e2571fa02e4f4e
zyqunix <117040076+zyqunix@users.noreply.github.com>
2025-02-24 17:02:38 +0100

    Update speechbubble.js

--- commands/Image/speechbubble.js
+++ commands/Image/speechbubble.js
@@ -5,30 +5,24 @@ module.exports = {
   name:'speechbubble',
   description: 'Remove top pixels from an image to make it look like a speech bubble',
   execute(message, args) {
-    // Check if the message has an attachment
     if (!message.attachments.size) {
       return message.edit('Please attach an image to convert!');
     }
 
-    // Get the attachment
     const attachment = message.attachments.first();
 
-    // Check if the attachment is an image
     if (!attachment.name.endsWith('.png') &&!attachment.name.endsWith('.jpg')) {
       return message.edit('Only PNG and JPG images are supported!');
     }
 
-    // Download the attachment
     axios.get(attachment.url, { responseType: 'arraybuffer' })
  .then(response => {
-        // Get the dimensions of the original image
         sharp(response.data)
     .metadata()
     .then(metadata => {
             const width = metadata.width;
             const height = metadata.height;
 
-            // Create a mask image with a white rectangle at the top
             sharp({
               create: {
                 width,
@@ -43,7 +37,7 @@ module.exports = {
               left: 0,
               top: 0,
               width,
-              height: 50 // adjust the value to remove more or less pixels
+              height: 50
             }
           ])
         .toFormat('png')
@@ -53,7 +47,6 @@ module.exports = {
                   return message.edit('Error processing image!');
                 }
 
-                // Composite the mask image onto the original image
                 sharp(response.data)
             .composite([{ input: maskBuffer, blend: 'dest-out' }])
             .toFormat('png')
@@ -63,7 +56,6 @@ module.exports = {
                     return message.edit('Error processing image!');
                   }
 
-                  // Send the modified image
                   message.edit({
                     files: [{
                       attachment: finalBuffer,
