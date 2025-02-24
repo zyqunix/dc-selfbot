2923bceacc9ccb57c18bde035ecf7be5ce8b4870
zyqunix <117040076+zyqunix@users.noreply.github.com>
2025-02-24 16:56:47 +0100

    Update rename.js

--- commands/Selfbot/rename.js
+++ commands/Selfbot/rename.js
@@ -11,22 +11,18 @@ module.exports = {
 
         const [oldCommandName, newCommandName] = args;
 
-        // Check if the old command exists
         const oldCommand = client.commands.get(oldCommandName);
         if (!oldCommand) {
             return message.edit(`Command \`${oldCommandName}\` does not exist.`).catch(console.error);
         }
 
-        // Check if the new command name is already taken
         if (client.commands.has(newCommandName)) {
             return message.edit(`Command \`${newCommandName}\` already exists.`).catch(console.error);
         }
 
-        // Rename the command
         client.commands.set(newCommandName, oldCommand);
         client.commands.delete(oldCommandName);
 
-        // Update the command name in the command object
         oldCommand.name = newCommandName;
 
         return message.edit(`Command \`${oldCommandName}\` has been renamed to \`${newCommandName}\`.`).catch(console.error);
